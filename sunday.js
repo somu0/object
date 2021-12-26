08aa2b108423392769c2784a7cbeb137ecd1ef19
somu0 <94071371+somu0@users.noreply.github.com>
2021-12-26 16:48:38 +0530

    Add files via upload

---
+++ sunday.js
@@ -0,0 +1,48 @@
+var g_class = {
+
+  'name': "mechanical",
+  
+  'instructor': {
+    
+  'name': "sekhar",
+
+   'subject': "manufacturing"
+
+  },
+
+  'students': [{
+      
+  'name': 'somu',
+      
+  'marks': 80,
+
+ },
+
+ {
+     
+ 'name': 'raju',
+
+ 'marks': 89,
+
+},
+
+{
+      
+   'name': 'manju',
+
+    'marks': 77,
+
+}
+
+}
+ 
+
+for(var i=0; i<g_class.students.length;i++)
+
+{
+    
+  if(g_class.students[i].marks > 85)
+        
+  console.log(g_class.students[i].name);
+
+}   
